fff8102d35d6cd738f8e355cbb6561a5e2b1ae01
S-Development <169008855+5ec0ur@users.noreply.github.com>
2024-12-22 04:12:21 +0000

    Delete items directory

--- items/qb.lua
+++
@@ -1,4 +0,0 @@
--- Add To qbcore/shared/items.lua
-
-
-amphetamine = { name = "amphetamine",   label = "Amphetamine",  weight = 1, type = "item",  image = "amphetamine.png",  unique = false, useable = true, shouldClose = true, combinable = nil,   description = "A powerful stimulant."},
