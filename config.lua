296e21177b7e74bcb0f0a5c0d778e45b8e9d4cb2
S-Development <169008855+5ec0ur@users.noreply.github.com>
2024-12-22 04:12:05 +0000

    Delete config.lua

--- config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-Config.Drugs = {
-    -- Adjust this to your liking   
-    ["amphetamine"] = { ingredients = { "cokebaggy", "joint" }, result = "amphetamine" },
-    ["drug2"] = { ingredients = { "ingredient3", "ingredient4" }, result = "drug2_result" }
-}
