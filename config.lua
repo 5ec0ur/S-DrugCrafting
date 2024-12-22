4650a7aea0f50103e10edc1dc17d057cab684a2d
S-Development <169008855+5ec0ur@users.noreply.github.com>
2024-12-22 03:20:44 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.Drugs = {
+    -- Adjust this to your liking   
+    ["amphetamine"] = { ingredients = { "cokebaggy", "joint" }, result = "amphetamine" },
+    ["drug2"] = { ingredients = { "ingredient3", "ingredient4" }, result = "drug2_result" }
+}
